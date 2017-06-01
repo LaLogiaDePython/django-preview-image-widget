8f74720d9c4c9f21c68481807b612a1364b7d42e
yamijuan <juandgoc@gmail.com>
2017-05-31 19:44:20 -0500

    Renamed simple_image_widget to preview_image_widget

--- preview_image_widget/widgets.py
+++ preview_image_widget/widgets.py
@@ -2,29 +2,22 @@ from django.conf import settings
 from django.forms import FileInput
 
 
-class SimpleImageWidget(FileInput):
+class PreviewImageWidget(FileInput):
     """
     Widget for displaying a image-preview in file fields.
     """
     def __init__(self, *args, **kwargs):
         """
-        Adds a
+        Adds a 'preview_image_widget' attr for the input
         """
         kwargs['attrs'] = {
-            'simple_image_widget': 1
+            'preview_image_widget': 1
         }
-        super(SimpleImageWidget, self).__init__(*args, **kwargs)
+        super(PreviewImageWidget, self).__init__(*args, **kwargs)
 
     def render(self, name, value, attrs=None):
         """
-        Autor: Milton Lenis
-        Fecha: Abril 2 2017
-        Se sobrecarga el método render para agregar la imágen a la vista previa al editar en caso de que tenga un valor
-        almacenado
-        :param name:
-        :param value:
-        :param attrs:
-        :return:
+        Adds the previous value of the file to an attr, if there is any, for preview.
         """
         if value:
             preview = {"data-preview": settings.MEDIA_URL + str(value)}
@@ -32,10 +25,10 @@ class SimpleImageWidget(FileInput):
                 attrs.update(preview)
             else:
                 attrs = preview
-        return super(SimpleImageWidget, self).render(name, value, attrs=attrs)
+        return super(PreviewImageWidget, self).render(name, value, attrs=attrs)
 
     class Media:
         css = {
-            'all': ('plugins/simple_image_widget/simple_image_widget.css',)
+            'all': ('plugins/preview_image_widget/preview_image_widget.css',)
         }
-        js = ('plugins/simple_image_widget/simple_image_widget.js',)
+        js = ('plugins/preview_image_widget/preview_image_widget.js',)

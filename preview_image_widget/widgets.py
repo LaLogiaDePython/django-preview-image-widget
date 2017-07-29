276d2f108ed6f2083e1054d9a52fa62bf64844db
yamijuan <juandgoc@gmail.com>
2017-07-29 10:37:43 -0500

    Code improvements

--- preview_image_widget/widgets.py
+++ preview_image_widget/widgets.py
@@ -16,16 +16,16 @@ class PreviewImageWidget(FileInput):
         }
         super(PreviewImageWidget, self).__init__(*args, **kwargs)
 
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, **kwargs):
         """
         Adds the previous value of the file to an attr, if there is any, for preview.
         """
         if value:
             preview = {"data-preview": settings.MEDIA_URL + str(value)}
-            if not attrs:
-                attrs = {}
-            attrs.update(preview)
-        return super(PreviewImageWidget, self).render(name, value, attrs=attrs)
+            if not kwargs.get('attrs'):
+                kwargs['attrs'] = {}
+            kwargs['attrs'].update(preview)
+        return super(PreviewImageWidget, self).render(name, value, **kwargs)
 
     class Media:
         css = {
